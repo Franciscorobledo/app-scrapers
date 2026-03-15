fe73e825223412b60574a863f4ac1d7573fba092
Franciscorobledo <97235822+Franciscorobledo@users.noreply.github.com>
2026-03-15 20:30:59 -0300

    Create scraper utility functions

---
+++ app/scraper/utils.py
@@ -0,0 +1,18 @@
+# Scraper Utility Functions
+
+def extract_data(response):
+    """Extracts relevant data from the response object."""
+    # Implementation code here
+    pass
+
+
+def save_data(data, filename):
+    """Saves extracted data to a file."""
+    # Implementation code here
+    pass
+
+
+def log_extraction_info(extraction_details):
+    """Logs details about the data extraction process."""
+    # Implementation code here
+    pass
